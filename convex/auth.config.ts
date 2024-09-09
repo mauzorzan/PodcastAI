b92da74559f3869785ff5dbc72c6228a4fcafc31
mauriciozorzan <dr.maukz@gmail.com>
2024-09-09 15:44:39 +0800

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://ace-antelope-41.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
